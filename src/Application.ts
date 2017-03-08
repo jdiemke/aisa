43df7833ada57473748356f95e5006ea5dc16fde
Johannes Diemke <johannesdiemke128@gmail.com>
2017-03-08 23:59:16 +0100

    Initial commit.

---
+++ src/Application.ts
@@ -0,0 +1,10 @@
+
+class Application {
+
+    public main(): void {
+        console.log('Hello World!');
+    }
+
+}
+
+new Application().main();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        application: './src/Application.ts'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: './dist'
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    configFileName: './src/tsconfig.json'
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
