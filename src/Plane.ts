3a7798174f389156e55aae704cd8e3dc284aba77
Johannes Diemke <johannesdiemke128@gmail.com>
2017-05-15 00:00:24 +0200

    Added a plance class for primitive clipping.

---
+++ src/Plane.ts
@@ -0,0 +1,13 @@
+/**
+ * Defines a plane as used for primitive clipping against the
+ * view frustum. 
+ */
+
+import Vector3 from './Vector3f';
+
+export default class Plane {
+
+    private distance: number;
+    private normal: Vector3;
+
+}
