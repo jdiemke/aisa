9378642552b1675492a552f82765fa3a4cb1e0c7
Johannes Diemke <johannes.diemke@eventim.de>
2018-05-17 01:42:56 +0200

    Started moving effects into separate files.

--- src/MetalHeadzScene.ts
+++ src/MetalHeadzScene.ts
@@ -7,14 +7,9 @@ import Texture from './Texture';
 
 const metalJson = require('./assets/metalheadz.json');
 
-export class MetalHeadzScene extends AbstractScene {
+class SkyBox {
 
-    private razorLogo: Texture;
-    private metalheadz: Texture;
-    private texture11: Texture;
-    private texture13: Texture;
-    private noise: Texture;
-    private dirt: Texture;
+    // TODO: make single textures
     private skybox: {
         back?: Texture,
         down?: Texture,
@@ -23,6 +18,82 @@ export class MetalHeadzScene extends AbstractScene {
         right?: Texture,
         up?: Texture
     } = {};
+
+
+    // move code from framebuffer into draw method!
+    public draw(framebuffer: Framebuffer, mv: Matrix4f): void {
+        framebuffer.drawSkyBox(mv.getRotation(), this.skybox);
+    }
+
+    public init(): Promise<any> {
+        return Promise.all([
+        this.createTexture(require('./assets/skybox/skybox_back.png'), false).then(
+            (texture: Texture) => this.skybox.back = texture
+        ),
+        this.createTexture(require('./assets/skybox/skybox_down.png'), false).then(
+            (texture: Texture) => this.skybox.down = texture
+        ),
+        this.createTexture(require('./assets/skybox/skybox_front.png'), false).then(
+            (texture: Texture) => this.skybox.front = texture
+        ),
+        this.createTexture(require('./assets/skybox/skybox_left.png'), false).then(
+            (texture: Texture) => this.skybox.left = texture
+        ),
+        this.createTexture(require('./assets/skybox/skybox_right.png'), false).then(
+            (texture: Texture) => this.skybox.right = texture
+        ),
+        this.createTexture(require('./assets/skybox/skybox_up.png'), false).then(
+            (texture: Texture) => this.skybox.up = texture
+        )]);
+    }
+
+    // TODO: make TextureUtils
+    public createTexture(path: string, hasAlpha: boolean): Promise<Texture> {
+        return new Promise<Texture>((resolve) => {
+            const img = new Image();
+            img.onload = () => {
+                const texture = new Texture();
+                texture.texture = this.getImageData(img, hasAlpha);
+                texture.width = img.width;
+                texture.height = img.height;
+                resolve(texture);
+            };
+            img.onerror = () => resolve();
+            img.src = path;
+        });
+    }
+
+    getImageData(image: HTMLImageElement, withAlpha: boolean = false): Uint32Array {
+        let canvas: HTMLCanvasElement = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        let context: CanvasRenderingContext2D = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+        let data = context.getImageData(0, 0, image.width, image.height).data;
+        let conv = new Uint32Array(data.length / 4);
+        let c = 0;
+        for (let i = 0; i < data.length; i += 4) {
+            if (withAlpha) {
+                conv[c] = (data[i + 3] << 24) | (data[i + 2] << 16) | (data[i + 1] << 8) | data[i + 0];
+            } else {
+                conv[c] = (255 << 24) | (data[i + 2] << 16) | (data[i + 1] << 8) | data[i + 0];
+            }
+
+            c++;
+        }
+        return conv;
+    }
+
+}
+export class MetalHeadzScene extends AbstractScene {
+
+    private metalheadz: Texture;
+    private texture11: Texture;
+    private texture13: Texture;
+    private noise: Texture;
+    private dirt: Texture;
+    private skyBox: SkyBox;
+  
     private blenderObjMetal: any;
 
     private accumulationBuffer: Uint32Array = new Uint32Array(320 * 200);
@@ -30,31 +101,15 @@ export class MetalHeadzScene extends AbstractScene {
     public init(framebuffer: Framebuffer): Promise<any> {
         framebuffer.setCullFace(CullFace.BACK);
         this.blenderObjMetal = framebuffer.getBlenderScene(metalJson, false);
+        this.skyBox = new SkyBox();
+        // TODO:
+        // make classes for assets
+        // skybox, lens flare, 3d modell
 
         return Promise.all([
-            this.createTexture(require('./assets/razor1911.png'), true).then(
-                (texture: Texture) => this.razorLogo = texture
-            ),
+            this.skyBox.init(),
             this.createTexture(require('./assets/metalheadz.png'), false).then(
                 (texture: Texture) => this.metalheadz = texture
-            ),
-            this.createTexture(require('./assets/skybox/skybox_back.png'), false).then(
-                (texture: Texture) => this.skybox.back = texture
-            ),
-            this.createTexture(require('./assets/skybox/skybox_down.png'), false).then(
-                (texture: Texture) => this.skybox.down = texture
-            ),
-            this.createTexture(require('./assets/skybox/skybox_front.png'), false).then(
-                (texture: Texture) => this.skybox.front = texture
-            ),
-            this.createTexture(require('./assets/skybox/skybox_left.png'), false).then(
-                (texture: Texture) => this.skybox.left = texture
-            ),
-            this.createTexture(require('./assets/skybox/skybox_right.png'), false).then(
-                (texture: Texture) => this.skybox.right = texture
-            ),
-            this.createTexture(require('./assets/skybox/skybox_up.png'), false).then(
-                (texture: Texture) => this.skybox.up = texture
             ),
             this.createTexture(require('./assets/ring.png'), true).then(
                 (texture: Texture) => this.texture11 = texture
@@ -77,18 +132,12 @@ export class MetalHeadzScene extends AbstractScene {
 
         framebuffer.clearDepthBuffer();
 
-        const camera: Matrix4f =
-            Matrix4f.constructTranslationMatrix(0, 0, -134 + (Math.sin(elapsedTime * 0.00007) * 0.5 + 0.5) * 17)
-            .multiplyMatrix(
-                Matrix4f.constructXRotationMatrix(elapsedTime * 0.0008).multiplyMatrix(
-                    Matrix4f.constructYRotationMatrix(-elapsedTime * 0.0009).multiplyMatrix(
-                        Matrix4f.constructTranslationMatrix(0, 0, 0)
-                    )));
-
+        const camera: Matrix4f = this.computeCameraMovement(elapsedTime);
         const mv: Matrix4f = camera.multiplyMatrix(Matrix4f.constructScaleMatrix(7, 7, 7));
 
-        framebuffer.drawSkyBox(mv.getRotation(), this.skybox);
-     
+        // put skybox here
+        this.skyBox.draw(framebuffer, mv);
+
         framebuffer.clearDepthBuffer();
         framebuffer.setBob(this.metalheadz);
         const scal: number = 1.0;
@@ -130,6 +179,17 @@ export class MetalHeadzScene extends AbstractScene {
             texture.height = 256;
             resolve(texture);
         });
+    }
+
+    private computeCameraMovement(elapsedTime: number): Matrix4f {
+        return Matrix4f.constructTranslationMatrix(0, 0, -134 + (Math.sin(elapsedTime * 0.00007) * 0.5 + 0.5) * 17)
+            .multiplyMatrix(
+                Matrix4f.constructXRotationMatrix(elapsedTime * 0.0008).multiplyMatrix(
+                    Matrix4f.constructYRotationMatrix(-elapsedTime * 0.0009).multiplyMatrix(
+                        Matrix4f.constructTranslationMatrix(0, 0, 0)
+                    )
+                )
+            );
     }
 
 }
