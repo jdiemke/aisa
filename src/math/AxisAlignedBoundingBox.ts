7fe743ee6716a9a84b5b321a5d3f6a38fb19c9d3
Johannes Diemke <johannes.diemke@eventim.de>
2018-05-27 22:52:57 +0200

    Adds aabb class

---
+++ src/math/AxisAlignedBoundingBox.ts
@@ -0,0 +1,28 @@
+import { BoundingVolume } from './BoundingVolume';
+import { Plane } from './Plane';
+import { Vector3f } from './Vector3f';
+
+export class AxisAlignedBoundingBox implements BoundingVolume {
+
+    private min: Vector3f;
+    private max: Vector3f;
+
+    public constructor(min: Vector3f, max: Vector3f) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public intersects(aabb: AxisAlignedBoundingBox): boolean {
+        return (this.min.x < aabb.max.x && this.min.y < aabb.max.y && this.min.z < aabb.max.z &&
+                this.max.x > aabb.min.x && this.max.y > aabb.min.y && this.max.z < aabb.min.z);
+    }
+
+    /**
+     * @see https://fgiesen.wordpress.com/2010/10/17/view-frustum-culling/
+     * @param plane
+     */
+    public isInsidePositiveHalfSpace(plane: Plane): boolean {
+        throw new Error('Method not implemented.');
+    }
+
+}
