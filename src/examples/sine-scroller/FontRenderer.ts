87f27b822e88d9cbf701ba5270230f823dbee706
Johannes Diemke <johannesdiemke128@gmail.com>
2018-09-04 14:19:52 +0200

    Enhance sine scroller

--- src/examples/sine-scroller/FontRenderer.ts
+++ src/examples/sine-scroller/FontRenderer.ts
@@ -4,19 +4,11 @@ import { Texture, TextureUtils } from '../../texture';
 export class FontRenderer {
 
     private fontTexture: Texture;
-    private witdh: number = 32;
-    private height: number = 34;
     private charToIndex: Map<number, number>;
 
-    constructor(private framebuffer: Framebuffer, private file: string) {
+    constructor(private framebuffer: Framebuffer, private width: number,
+                private height: number, fonts: string,  private file: string) {
         this.charToIndex = new Map<number, number>();
-
-        const fonts: string =
-            'ABCDEFGHIJ' +
-            'KLMNOPQRST' +
-            'UVWXYZ@+# ' +
-            '0123456789' +
-            '!\'()?-/.,';
 
         for (let x: number = 0; x < fonts.length; x++) {
             this.addCharInex(fonts.charCodeAt(x), x);
@@ -35,32 +27,33 @@ export class FontRenderer {
         this.framebuffer.drawTextureRectFastAlpha(0, 0, 0, 0, 50, 50, this.fontTexture);
     }
 
-    public drawText(x: number, y: number, text: string, time: number): void {
+    public drawText(x: number, y: number, text: string, time: number, sine: boolean = true): void {
         let xpos: number = x;
         const firstIndex: number = 'A'.charCodeAt(0);
-        const xFonts: number = this.fontTexture.width / this.witdh;
+        const xFonts: number = this.fontTexture.width / this.width;
 
         const speed: number = 0.07;
-        const xOff: number = Math.floor(time * speed) % this.witdh;
-        const textOff: number = Math.floor((time * speed) / this.witdh) % text.length;
+        const xOff: number = Math.floor(time * speed) % this.width;
+        const textOff: number = Math.floor((time * speed) / this.width) % text.length;
         xpos -= xOff;
-        for (let i: number = 0; i < Math.floor(320 / this.witdh + 1); i++) {
+        for (let i: number = 0; i < Math.floor(320 / this.width + 1); i++) {
             const asciiCode: number = text.charCodeAt((i + textOff) % text.length);
             const index: number = this.charToIndex.has(asciiCode) ? this.charToIndex.get(asciiCode) : 0;
-            const tx: number = Math.floor(index % xFonts) * this.witdh;
+            const tx: number = Math.floor(index % xFonts) * this.width;
             const ty: number = Math.floor(index / xFonts) * this.height;
             this.drawTextureRectFastAlpha(xpos,
-                y, tx, ty, this.witdh, this.height, this.fontTexture, time);
-            xpos += this.witdh;
+                y, tx, ty, this.width, this.height, this.fontTexture, time, sine);
+            xpos += this.width;
         }
     }
 
     public drawTextureRectFastAlpha(xs: number, ys: number, xt: number, yt: number,
-        width: number, height: number, texture: Texture, time: number): void {
+                                    width: number, height: number, texture: Texture, time: number, sine: boolean = true): void {
         const startW: number = Math.max(0, 0 - xs);
         const endW: number = Math.min(xs + width, 320) - xs;
         for (let w: number = startW; w < endW; w++) {
-            const yDisp: number = Math.round(Math.sin(time * 0.004 + (xs + w) * 0.013) * 40);
+            
+            const yDisp: number = sine ? Math.round(Math.sin(time * 0.004 + (xs + w) * 0.013) * 30) : 0;
             let texIndex: number = xt + w + yt * texture.width;
             let frIndex: number = xs + w + (ys + yDisp) * 320;
 
