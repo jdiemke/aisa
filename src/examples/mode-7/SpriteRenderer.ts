595bdd3acaec242e46b0997cc81950eee9c0bd12
Johannes Diemke <johannesdiemke128@gmail.com>
2018-11-02 23:48:19 +0100

    Add proper sprite ordering

---
+++ src/examples/mode-7/SpriteRenderer.ts
@@ -0,0 +1,30 @@
+import { Framebuffer } from '../../Framebuffer';
+import { Sprite } from './Sprite';
+
+export class SpriteRenderer {
+
+    private sprites: Array<Sprite>;
+
+    constructor() {
+        this.sprites = new Array<Sprite>();
+    }
+
+    public addSprite(sprite: Sprite): void {
+        this.sprites.push(sprite);
+    }
+
+    public render(framebuffer: Framebuffer): void {
+        this.sprites.sort((a: Sprite, b: Sprite) => b.z - a.z);
+
+        this.sprites.forEach((sp: Sprite) => {
+            framebuffer.scaleClipBlitter.drawScaledTextureClip(
+                sp.xp,
+                sp.yp,
+                sp.width,
+                sp.height, sp.texture, sp.alphaBlend);
+        });
+        this.sprites.length = 0;
+
+    }
+
+}
