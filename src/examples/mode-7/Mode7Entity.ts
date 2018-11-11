a4fadf2fd333e160afea84b9fb97af49fd421374
Johannes Diemke <johannesdiemke128@gmail.com>
2018-11-11 23:08:12 +0100

    Add minimap HUD

--- src/examples/mode-7/Mode7Entity.ts
+++ src/examples/mode-7/Mode7Entity.ts
@@ -6,14 +6,20 @@ export abstract class Mode7Entity {
 
     public position: Vector2f;
     public height: number;
+    public priority: number;
 
-    public constructor(position: Vector2f, height: number = 0) {
+    public constructor(position: Vector2f, height: number = 0, priority: number = 0) {
         this.position = position;
         this.height = height;
+        this.priority = priority;
     }
 
     public abstract getTexture(camera: Camera): Texture;
 
     public abstract getAlpha(): number;
+
+    public getPriority(): number {
+        return this.priority;
+    }
 
 }
