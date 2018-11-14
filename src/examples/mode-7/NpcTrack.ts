a34a145714a598e9de0945852b1632db2e0a78db
Johannes Diemke <johannesdiemke128@gmail.com>
2018-11-15 00:06:25 +0100

    Move npc track into helper class

---
+++ src/examples/mode-7/NpcTrack.ts
@@ -0,0 +1,53 @@
+import { Vector2f } from '../../math/index';
+
+export class NpcTrack {
+
+    public static readonly track: Array<Vector2f> = [
+        new Vector2f(920, 580),
+        new Vector2f(940, 476),
+        new Vector2f(922, 415),
+        new Vector2f(850, 370),
+        new Vector2f(766, 323),
+        new Vector2f(684, 277),
+        new Vector2f(615, 250),
+        new Vector2f(524, 209),
+        new Vector2f(449, 165),
+        new Vector2f(371, 123),
+        new Vector2f(295, 91),
+        new Vector2f(229, 70),
+        new Vector2f(165, 68),
+        new Vector2f(119, 89),
+        new Vector2f(85, 133),
+        new Vector2f(64, 190),
+        new Vector2f(56, 247),
+        new Vector2f(72, 293),
+        new Vector2f(87, 348),
+        new Vector2f(81, 408),
+        new Vector2f(71, 477),
+        new Vector2f(64, 572),
+        new Vector2f(67, 637),
+        new Vector2f(78, 718),
+        new Vector2f(134, 750),
+        new Vector2f(226, 727),
+        new Vector2f(286, 669),
+        new Vector2f(344, 629),
+        new Vector2f(447, 586),
+        new Vector2f(535, 588),
+        new Vector2f(583, 645),
+        new Vector2f(612, 712),
+        new Vector2f(641, 786),
+        new Vector2f(681, 849),
+        new Vector2f(744, 895),
+        new Vector2f(820, 912),
+        new Vector2f(880, 915),
+        new Vector2f(930, 880),
+        new Vector2f(955, 804),
+        new Vector2f(944, 717),
+        new Vector2f(921, 643)
+    ];
+
+    private constructor() {
+
+    }
+
+}
