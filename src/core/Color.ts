fb2670b789ec1838e6976e162dac46216c3e1a64
Johannes Diemke <johannesdiemke128@gmail.com>
2018-11-22 01:29:50 +0100

    Add raycasting scaffold

--- src/core/Color.ts
+++ src/core/Color.ts
@@ -1,6 +1,6 @@
 /**
  * Color
- * 
+ *
  * @export
  * @class Color
  * @see https://www.rapidtables.com/web/color/RGB_Color.html
@@ -13,11 +13,12 @@ export class Color {
     public static readonly RED = new Color(255, 0, 0, 255);
     public static readonly LIME = new Color(0, 255, 0, 255);
     public static readonly BLUE = new Color(0, 0, 255, 255);
+    public static readonly DARK_BLUE = new Color(0, 0, 64, 255);
     public static readonly YELLOW = new Color(255, 255, 0, 255);
     public static readonly CYAN = new Color(0, 255, 255, 255);
     public static readonly MAGENTA = new Color(255, 0, 255, 255);
-    public static readonly SLATE_GRAY = new Color(112,128,144, 255);
-    
+    public static readonly SLATE_GRAY = new Color(112, 128, 144, 255);
+
     constructor(private r: number, private g: number, private b: number, private a: number) {
     }
 
