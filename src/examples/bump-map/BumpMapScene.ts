eed572b3a5bd0d0659279f752698a36cf319e003
Johannes Diemke <johannesdiemke128@gmail.com>
2018-11-29 21:50:28 +0100

    Optimize 2d bump mapping

--- src/examples/bump-map/BumpMapScene.ts
+++ src/examples/bump-map/BumpMapScene.ts
@@ -16,7 +16,8 @@ export class BumpMapScene extends AbstractScene {
     private map: Texture;
     private bump: Texture;
     private phong: Texture;
-    private fontRenderer2: FontRenderer;
+    private normals = Array<[number, number]>(320 * 200);
+
 
     public init(framebuffer: Framebuffer): Promise<any> {
         this.phong = new Texture(new Uint32Array(256 * 256), 256, 256);
@@ -33,57 +34,60 @@ export class BumpMapScene extends AbstractScene {
                     inten << 8 | inten << 16 | 255 << 24;
             }
         }
-        const fonts: string =
-            'ABCDEFGHIJ' +
-            'KLMNOPQRST' +
-            'UVWXYZ@+# ' +
-            '0123456789' +
-            '!\'()?-/.,';
-        this.fontRenderer2 = new FontRenderer(
-            framebuffer,
-            32, 34, fonts,
-            require('../sine-scroller/assets/fraxionFont.png')
-        );
+
 
         return Promise.all([
             TextureUtils.load(require('./map.png'), false).then(texture => this.map = texture),
             TextureUtils.load(require('./bump.png'), false).then(texture => this.bump = texture),
-            this.fontRenderer2.init()
+
         ]);
+    }
+    public onInit(): void {
+         // precompute normal map
+
+         let framebufferIndex: number = 0;
+         for (let y = 0; y < 200; y++) {
+             for (let x = 0; x < 320; x++) {
+                 let nx = ((this.bump.getPixel3(this.bump, x - 1, y) & 0xff) - (this.bump.getPixel3(this.bump, x + 1, y) & 0xff));
+                 let ny = ((this.bump.getPixel3(this.bump, x, y - 1) & 0xff) - (this.bump.getPixel3(this.bump, x, y + 1) & 0xff));
+                 this.normals[framebufferIndex++] = [nx, ny];
+             }
+         }
+
     }
 
     public render(framebuffer: Framebuffer): void {
-        let elapsedTime: number = Date.now() ;
+        const elapsedTime: number = Date.now();
         framebuffer.fastFramebufferCopy(framebuffer.framebuffer, this.bump.texture);
 
-        for (let x = 0; x < 320; x++) {
-            for (let y = 0; y < 200; y++) {
-                let lx = x - Math.sin(2 * Date.now() * 0.0008) * 160 - 160;
-                let ly = y - Math.sin(3 * Date.now() * 0.0008) * 100 - 100;
-                let nx = ((this.bump.getPixel3(this.bump, x - 1, y) & 0xff) - (this.bump.getPixel3(this.bump, x + 1, y) & 0xff));
-                let ny = ((this.bump.getPixel3(this.bump, x, y - 1) & 0xff) - (this.bump.getPixel3(this.bump, x, y + 1) & 0xff));
+        let framebufferIndex: number = 0;
+        const lightPosX = - Math.sin(2 * elapsedTime * 0.0008) * 160 - 160;
+        const lightPosY = - Math.sin(3 * elapsedTime * 0.0008) * 100 - 100;
+        for (let y = 0; y < 200; y++) {
+            for (let x = 0; x < 320; x++) {
 
-                nx = Math.round((-nx - lx) + 128);
-                ny = Math.round((-ny - ly) + 128);
+                let lx = x + lightPosX;
+                let ly = y + lightPosY;
+
+                const normal = this.normals[framebufferIndex];
+
+                let nx = ((-normal[0] - lx) + 128) | 0;
+                let ny = ((-normal[1] - ly) + 128) | 0;
                 let pixel = this.phong.getPixel2(this.phong,
                     Math.max(0, Math.min(nx, 255)), Math.max(0, Math.min((ny), 255)));
                 let scale = Math.min((pixel & 0xff) + 80, 255) / 255;
 
-                let texel = this.map.getPixel2(this.map, x, y);
+                let texel = this.map.texture[framebufferIndex];
                 let red = (texel >> 0) & 0xff;
                 let green = (texel >> 8) & 0xff;
                 let blue = (texel >> 16) & 0xff;
-                framebuffer.drawPixel(x, y,
+                framebuffer.framebuffer[framebufferIndex++] =
                     Math.round(red * scale) |
                     Math.round(green * scale) << 8 |
-                    Math.round(blue * scale) << 16 | 255 << 24
-                );
+                    Math.round(blue * scale) << 16 | 255 << 24;
             }
         }
 
-        this.fontRenderer2.drawText(0, 100,
-            '   * WE REALLY LOVE SCROLLERS * HOW ABOUT YOU? THIS PRODUCTION IS FROM HOODLUM' +
-            '~< LETS GO ON WITH THE GENERAL BLAH BLAH      ', elapsedTime * 1.4, true);
     }
 
 }
