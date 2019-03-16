ec6b16a901ecc31a903f287d552d3431582a33d4
Johannes Diemke <johannesdiemke128@gmail.com>
2019-03-16 16:43:58 +0100

    Add damping to camera

--- src/math/Vector3f.ts
+++ src/math/Vector3f.ts
@@ -10,63 +10,63 @@ export class Vector3f {
         this.z = z;
     }
 
-    public mult(scale: number) {
+    public mult(scale: number): Vector3f {
         return new Vector3f(this.x * scale, this.y * scale, this.z * scale);
     }
 
-    sub(vec: Vector3f): Vector3f {
+    public sub(vec: Vector3f): Vector3f {
         return new Vector3f(this.x - vec.x, this.y - vec.y, this.z - vec.z);
     }
 
-    sub2(vec1: Vector3f, vec2: Vector3f): void {
+    public sub2(vec1: Vector3f, vec2: Vector3f): void {
         this.x = vec1.x - vec2.x;
         this.y = vec1.y - vec2.y;
         this.z = vec1.z - vec2.z;
     }
 
-    mul(scal: number): Vector3f {
+    public mul(scal: number): Vector3f {
         return new Vector3f(this.x * scal, this.y * scal, this.z * scal);
     }
 
-    add(vec: Vector3f): Vector3f {
+    public add(vec: Vector3f): Vector3f {
         return new Vector3f(this.x + vec.x, this.y + vec.y, this.z + vec.z);
     }
 
-    add2(vec: Vector3f, vec2: Vector3f): void {
+    public add2(vec: Vector3f, vec2: Vector3f): void {
         this.x = vec.x + vec2.x;
         this.y = vec.y + vec2.y;
         this.z = vec.z + vec2.z;
     }
 
-    cross(vec: Vector3f): Vector3f {
+    public cross(vec: Vector3f): Vector3f {
         return new Vector3f(this.y * vec.z - this.z * vec.y,
             this.z * vec.x - this.x * vec.z,
             this.x * vec.y - this.y * vec.x);
     }
 
-    cross2(vec1: Vector3f, vec2: Vector3f): void {
+    public cross2(vec1: Vector3f, vec2: Vector3f): void {
         this.x = vec1.y * vec2.z - vec1.z * vec2.y;
         this.y = vec1.z * vec2.x - vec1.x * vec2.z;
         this.z = vec1.x * vec2.y - vec1.y * vec2.x;
     }
 
-    length() {
+    public length(): number {
         return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
-    normalize(): Vector3f {
-        let reci = 1.0 / this.length();
+    public normalize(): Vector3f {
+        const reci: number = 1.0 / this.length();
         return this.mul(reci);
     }
 
-    normalize2(): void {
-        let reci = 1.0 / this.length();
+    public normalize2(): void {
+        const reci: number = 1.0 / this.length();
         this.x *= reci;
         this.y *= reci;
         this.z *= reci;
     }
 
-    dot(vec: Vector3f): number {
+    public dot(vec: Vector3f): number {
         return this.x * vec.x + this.y * vec.y + this.z * vec.z;
     }
 
