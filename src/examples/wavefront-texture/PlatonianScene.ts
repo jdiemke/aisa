5edf8dd9f2c75e071f70cd3973cac32ca699be6e
Johannes Diemke <johannesdiemke128@gmail.com>
2019-04-27 18:04:49 +0100

    Add new wavefront texture scene with baked lighting and shadows

--- src/examples/wavefront-texture/PlatonianScene.ts
+++ src/examples/wavefront-texture/PlatonianScene.ts
@@ -5,6 +5,7 @@ import { WavefrontLoader } from '../../model/wavefront-obj/WavefrontLoader';
 import { TexturedMesh } from '../../rendering-pipelines/TexturedMesh';
 import { AbstractScene } from '../../scenes/AbstractScene';
 import { Texture, TextureUtils } from '../../texture';
+import { Color } from '../../core/Color';
 
 /**
  * TODO: extract lens into effect class
@@ -26,13 +27,13 @@ export class PlatonianScene extends AbstractScene {
             TextureUtils.load(require('../../assets/blurredBackground.png'), false).then(
                 (texture: Texture) => this.blurred = texture
             ),
-            TextureUtils.load(require('../../assets/texture.png'), false).then(
+            TextureUtils.load(require('../../assets/baked_susanna.png'), false).then(
                 (texture: Texture) => this.platonian = texture
             ),
             TextureUtils.generateProceduralNoise().then(
                 (texture: Texture) => this.noise = texture
             ),
-            WavefrontLoader.loadWithTexture(require('../../assets/spike-ball-tex.obj')).then(
+            WavefrontLoader.loadWithTexture(require('../../assets/susanna.obj')).then(
                 (x: Array<TexturedMesh>) => this.platonianMesh = x
             )
         ]);
@@ -40,25 +41,17 @@ export class PlatonianScene extends AbstractScene {
 
     public render(framebuffer: Framebuffer): void {
         const time: number = Date.now();
-
-        framebuffer.fastFramebufferCopy(framebuffer.framebuffer, this.blurred.texture);
-
+        framebuffer.clearColorBuffer(Color.BLACK.toPackedFormat());
         this.drawBlenderScene8(framebuffer, time * 2);
-
-        const texture3: Texture = new Texture(this.accumulationBuffer, 320, 200);
-        framebuffer.drawTexture(0, 0, texture3, 0.75);
-        framebuffer.fastFramebufferCopy(this.accumulationBuffer, framebuffer.framebuffer);
-        framebuffer.noise(time, this.noise);
     }
 
     public drawBlenderScene8(framebuffer: Framebuffer, elapsedTime: number): void {
         framebuffer.clearDepthBuffer();
 
         const camera: Matrix4f =
-            Matrix4f.constructTranslationMatrix(0, 0, -64 + (Math.sin(elapsedTime * 0.00007) * 0.5 + 0.5) * 17)
-                .multiplyMatrix(
-                    Matrix4f.constructXRotationMatrix(elapsedTime * 0.00035).multiplyMatrix(
-                        Matrix4f.constructYRotationMatrix(-elapsedTime * 0.0003)));
+        Matrix4f.constructTranslationMatrix(18, 2, -74 )
+        .multiplyMatrix(
+                Matrix4f.constructYRotationMatrix(-elapsedTime * 0.0003));
 
         const mv: Matrix4f = camera.multiplyMatrix(Matrix4f.constructScaleMatrix(8, 8, 8));
 
