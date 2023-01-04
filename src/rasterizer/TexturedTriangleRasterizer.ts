ccb4c5fe6e6993c6a07946b976b83b003ef205e2
Johannes Diemke <johannesdiemke128@gmail.com>
2023-01-04 19:12:12 +0100

    Refactors texturing triangle rasterizer

--- src/rasterizer/TexturedTriangleRasterizer.ts
+++ src/rasterizer/TexturedTriangleRasterizer.ts
@@ -39,53 +39,35 @@ export class TexturedTriangleRasterizer {
             if (x > p2.position.x) {
                 this.fillLongRightTriangle2(framebuffer, p1, p2, p3);
             } else {
-                const tex = p1.textureCoordinate;
-                const tex2 = p2.textureCoordinate;
-                const tex3 = p3.textureCoordinate;
-
-                this.fillLongLeftTriangle2(
-                    framebuffer,
-                    p1.position,
-                    p2.position,
-                    p3.position,
-                    new Vector3f(tex.u, tex.v, 0),
-                    new Vector3f(tex2.u, tex2.v, 0),
-                    new Vector3f(tex3.u, tex3.v, 0)
-                );
+                this.fillLongLeftTriangle2(framebuffer,p1,p2,p3);
             }
         }
     }
 
     private fillLongRightTriangle2(framebuffer: Framebuffer, v1: Vertex, v2: Vertex, v3: Vertex): void {
+       
+        // left slope
         let yDistanceLeft = v2.position.y - v1.position.y;
-
-
-
-
-        const yDistanceRight = v3.position.y - v1.position.y;
-
         let slope1 = (v2.position.x - v1.position.x) / yDistanceLeft;
-        const slope2 = (v3.position.x - v1.position.x) / yDistanceRight;
-
         let tslope1u = (v2.textureCoordinate.u / v2.position.z - v1.textureCoordinate.u / v1.position.z) / yDistanceLeft;
-        const tslope2u = (v3.textureCoordinate.u / v3.position.z - v1.textureCoordinate.u / v1.position.z) / yDistanceRight;
-
         let tslope1v = (v2.textureCoordinate.v / v2.position.z - v1.textureCoordinate.v / v1.position.z) / yDistanceLeft;
-        const tslope2v = (v3.textureCoordinate.v / v3.position.z - v1.textureCoordinate.v / v1.position.z) / yDistanceRight;
-
         let zslope1 = (1 / v2.position.z - 1 / v1.position.z) / yDistanceLeft;
-        const zslope2 = (1 / v3.position.z - 1 / v1.position.z) / yDistanceRight;
-
         let curz1 = 1.0 / v1.position.z;
-        let curz2 = 1.0 / v1.position.z;
-
         let curu1 = v1.textureCoordinate.u / v1.position.z;
         let curv1 = v1.textureCoordinate.v / v1.position.z;
+        let xPosition = v1.position.x;
+
+        // right slope
+        const yDistanceRight = v3.position.y - v1.position.y;
+        const slope2 = (v3.position.x - v1.position.x) / yDistanceRight;
+        const tslope2u = (v3.textureCoordinate.u / v3.position.z - v1.textureCoordinate.u / v1.position.z) / yDistanceRight;
+        const tslope2v = (v3.textureCoordinate.v / v3.position.z - v1.textureCoordinate.v / v1.position.z) / yDistanceRight;
+        const zslope2 = (1 / v3.position.z - 1 / v1.position.z) / yDistanceRight;
+        let curz2 = 1.0 / v1.position.z;
         let curu2 = v1.textureCoordinate.u / v1.position.z;
         let curv2 = v1.textureCoordinate.v / v1.position.z;
-
-        let xPosition = v1.position.x;
         let xPosition2 = v1.position.x;
+
         let yPosition = v1.position.y;
 
         for (let i = 0; i < yDistanceLeft; i++) {
@@ -206,36 +188,35 @@ export class TexturedTriangleRasterizer {
         }
     }
 
+    fillLongLeftTriangle2(framebuffer: Framebuffer, v1: Vertex, v2: Vertex, v3: Vertex): void {
 
-    fillLongLeftTriangle2(framebuffer: Framebuffer, v1: Vector4f, v2: Vector4f, v3: Vector4f, t1: Vector3f, t2: Vector3f, t3: Vector3f): void {
+        let yDistanceRight = v2.position.y - v1.position.y;
+        const yDistanceLeft = v3.position.y - v1.position.y;
 
-        let yDistanceRight = v2.y - v1.y;
-        const yDistanceLeft = v3.y - v1.y;
+        let slope2 = (v2.position.x - v1.position.x) / yDistanceRight;
+        const slope1 = (v3.position.x - v1.position.x) / yDistanceLeft;
 
-        let slope2 = (v2.x - v1.x) / yDistanceRight;
-        const slope1 = (v3.x - v1.x) / yDistanceLeft;
+        const tslope1u = (v3.textureCoordinate.u / v3.position.z - v1.textureCoordinate.u / v1.position.z) / yDistanceLeft;
+        let tslope2u = (v2.textureCoordinate.u / v2.position.z - v1.textureCoordinate.u / v1.position.z) / yDistanceRight;
 
-        const tslope1u = (t3.x / v3.z - t1.x / v1.z) / yDistanceLeft;
-        let tslope2u = (t2.x / v2.z - t1.x / v1.z) / yDistanceRight;
-
-        const tslope1v = (t3.y / v3.z - t1.y / v1.z) / yDistanceLeft;
-        let tslope2v = (t2.y / v2.z - t1.y / v1.z) / yDistanceRight;
+        const tslope1v = (v3.textureCoordinate.v / v3.position.z - v1.textureCoordinate.v / v1.position.z) / yDistanceLeft;
+        let tslope2v = (v2.textureCoordinate.v / v2.position.z - v1.textureCoordinate.v / v1.position.z) / yDistanceRight;
 
 
-        let zslope2 = (1 / v2.z - 1 / v1.z) / yDistanceRight;
-        const zslope1 = (1 / v3.z - 1 / v1.z) / yDistanceLeft;
+        let zslope2 = (1 / v2.position.z - 1 / v1.position.z) / yDistanceRight;
+        const zslope1 = (1 / v3.position.z - 1 / v1.position.z) / yDistanceLeft;
 
-        let curz1 = 1.0 / v1.z;
-        let curz2 = 1.0 / v1.z;
+        let curz1 = 1.0 / v1.position.z;
+        let curz2 = 1.0 / v1.position.z;
 
-        let curu1 = t1.x / v1.z;
-        let curv1 = t1.y / v1.z;
-        let curu2 = t1.x / v1.z;
-        let curv2 = t1.y / v1.z;
+        let curu1 = v1.textureCoordinate.u / v1.position.z;
+        let curv1 = v1.textureCoordinate.v / v1.position.z;
+        let curu2 = v1.textureCoordinate.u / v1.position.z;
+        let curv2 = v1.textureCoordinate.v / v1.position.z;
 
-        let xPosition = v1.x;
-        let xPosition2 = v1.x;
-        let yPosition = v1.y;
+        let xPosition = v1.position.x;
+        let xPosition2 = v1.position.x;
+        let yPosition = v1.position.y;
 
         for (let i = 0; i < yDistanceRight; i++) {
             const length = Math.round(xPosition2) - Math.round(xPosition);
@@ -285,20 +266,20 @@ export class TexturedTriangleRasterizer {
             curv2 += tslope2v;
         }
 
-        yDistanceRight = v3.y - v2.y;
-        slope2 = (v3.x - v2.x) / yDistanceRight;
-        zslope2 = (1 / v3.z - 1 / v2.z) / yDistanceRight;
+        yDistanceRight = v3.position.y - v2.position.y;
+        slope2 = (v3.position.x - v2.position.x) / yDistanceRight;
+        zslope2 = (1 / v3.position.z - 1 / v2.position.z) / yDistanceRight;
 
-        tslope2u = (t3.x / v3.z - t2.x / v2.z) / yDistanceRight;
-        tslope2v = (t3.y / v3.z - t2.y / v2.z) / yDistanceRight;
+        tslope2u = (v3.textureCoordinate.u / v3.position.z - v2.textureCoordinate.u / v2.position.z) / yDistanceRight;
+        tslope2v = (v3.textureCoordinate.v / v3.position.z - v2.textureCoordinate.v / v2.position.z) / yDistanceRight;
 
-        curz2 = 1.0 / v2.z;
+        curz2 = 1.0 / v2.position.z;
 
-        curu2 = t2.x / v2.z;
-        curv2 = t2.y / v2.z;
+        curu2 = v2.textureCoordinate.u / v2.position.z;
+        curv2 = v2.textureCoordinate.v / v2.position.z;
 
-        xPosition2 = v2.x;
-        yPosition = v2.y;
+        xPosition2 = v2.position.x;
+        yPosition = v2.position.y;
 
         for (let i = 0; i < yDistanceRight; i++) {
             const length = Math.round(xPosition2) - Math.round(xPosition);
