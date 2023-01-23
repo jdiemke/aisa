0df1ced94ae57ecd02d50c693ce6287ec4a915a7
Johannes Diemke <johannesdiemke128@gmail.com>
2023-01-23 17:42:54 +0100

    Refactor parallax effect

--- src/examples/xenusion/ParallaxScrolliongScene.ts
+++ src/examples/xenusion/ParallaxScrolliongScene.ts
@@ -4,29 +4,39 @@ import { AbstractScene } from '../../scenes/AbstractScene';
 import { Texture } from '../../texture/Texture';
 import { TextureUtils } from '../../texture/TextureUtils';
 
-export class TitanEffectScene extends AbstractScene {
+interface ParallaxLayer {
+    image: Texture;
+    layerWidth: number;
+    positionY: number;
+    positionX: number;
+    speed: number;
+};
 
+export class ParallaxScrollingScene extends AbstractScene {
 
+    // Background Textures
     private BG_sky: Texture;
     private BG_city: Texture;
     private BG_dirt1: Texture;
     private BG_dirt2: Texture;
     private BG_dirt3: Texture;
     private BG_road: Texture;
+
+    // Trees & Lamp
+    private PR_tree1: Texture;
+    private PR_tree2: Texture;
+    private PR_lamp: Texture;
+
+    // Buildings
     private AR_oldhouse: Texture;
     private AR_grill: Texture;
+    private AR_haneu: Texture;
 
-    private PR_tree1: Texture;
-    private PR_tree2 : Texture;
-    private PR_lamp : Texture;
+    // Layer Definition
+    private parallaxLayers: Array<ParallaxLayer>;
 
-    private texture4 : Texture;
     public init(framebuffer: Framebuffer): Promise<any> {
-
         return Promise.all([
-            TextureUtils.load(require('../../assets/font.png'), true).then(
-                (texture: Texture) => this.texture4 = texture
-            ),
             TextureUtils.load(require('../../assets/xenusion/BG_sky.png'), true).then(
                 (texture: Texture) => this.BG_sky = texture
             ),
@@ -60,14 +70,30 @@ export class TitanEffectScene extends AbstractScene {
             TextureUtils.load(require('../../assets/xenusion/PR_lamp.png'), true).then(
                 (texture: Texture) => this.PR_lamp = texture
             ),
+            TextureUtils.load(require('../../assets/xenusion/AR_haneu.png'), true).then(
+                (texture: Texture) => this.AR_haneu = texture
+            ),
         ]);
+    }
+
+    public onInit(): void {
+        this.parallaxLayers = this.createParallaxLayers();
     }
 
     public render(framebuffer: Framebuffer, time: number): void {
         framebuffer.clearColorBuffer(Color.RED.toPackedFormat());
 
-        const mySpeed = 0.24 ;
-        const data = [
+        this.parallaxLayers.forEach(layer => {
+            const disp = (time * layer.speed) | 0;
+            framebuffer.drawTexture((0 - (disp % layer.layerWidth)+layer.positionX)|0,layer.positionY, layer.image, 1);
+            framebuffer.drawTexture((layer.layerWidth - (disp % layer.layerWidth)+layer.positionX)|0, layer.positionY, layer.image, 1);
+        });
+    }
+
+    private createParallaxLayers(): Array<ParallaxLayer> {
+        const mySpeed = 0.24;
+
+        return [
             {
                 image: this.BG_sky,
                 layerWidth: 600,
@@ -112,44 +138,78 @@ export class TitanEffectScene extends AbstractScene {
             },
             {
                 image: this.PR_tree1,
-                layerWidth: 600,
+                layerWidth: 1200,
                 positionY: 163 - this.PR_tree1.height,
                 positionX: 253,
                 speed: mySpeed * 0.8 * 0.8,
             },
             {
                 image: this.PR_tree2,
-                layerWidth: 601,
-                positionY: 160 - this.PR_tree2.height,
+                layerWidth: 1200,
+                positionY: 161 - this.PR_tree2.height,
                 positionX: 7,
                 speed: mySpeed * 0.8 * 0.8,
             },
             {
                 image: this.PR_tree2,
-                layerWidth: 600,
+                layerWidth: 1200,
                 positionY: 158 - this.PR_tree2.height,
                 positionX: 245,
                 speed: mySpeed * 0.8 * 0.8,
             },
             {
                 image: this.PR_tree2,
-                layerWidth: 600,
+                layerWidth: 1200,
                 positionY: 158 - this.PR_tree2.height,
                 positionX: 347,
                 speed: mySpeed * 0.8 * 0.8,
             },
             {
+                image: this.PR_tree1,
+                layerWidth: 1200,
+                positionY: 163 - this.PR_tree1.height,
+                positionX: 600+153,
+                speed: mySpeed * 0.8 * 0.8,
+            },
+            {
+                image: this.PR_tree1,
+                layerWidth: 1200,
+                positionY: 158 - this.PR_tree1.height,
+                positionX: 600+265,
+                speed: mySpeed * 0.8 * 0.8,
+            },
+            {
+                image: this.PR_tree2,
+                layerWidth: 1200,
+                positionY: 162 - this.PR_tree2.height,
+                positionX: 600 +25,
+                speed: mySpeed * 0.8 * 0.8,
+            },
+            {
+                image: this.PR_tree2,
+                layerWidth: 1200,
+                positionY: 158 - this.PR_tree2.height,
+                positionX: 600+96,
+                speed: mySpeed * 0.8 * 0.8,
+            },
+            {
                 image: this.AR_oldhouse,
-                layerWidth: 600,
-                positionY: 176 - this.AR_oldhouse.height,
+                layerWidth: 1200,
+                positionY: 177 - this.AR_oldhouse.height,
                 positionX: 52,
                 speed: mySpeed * 0.8 * 0.8,
             },
-
+            {
+                image: this.AR_haneu,
+                layerWidth: 1200,
+                positionY: 177 - this.AR_haneu.height,
+                positionX: 900,
+                speed: mySpeed * 0.8 * 0.8,
+            },
             {
                 image: this.AR_grill,
-                layerWidth: 600,
-                positionY: 176 - this.AR_grill.height,
+                layerWidth: 1200,
+                positionY: 177 - this.AR_grill.height,
                 positionX: 312,
                 speed: mySpeed * 0.8 * 0.8,
             },
@@ -160,7 +220,7 @@ export class TitanEffectScene extends AbstractScene {
                 positionX: 7,
                 speed: mySpeed * 0.8 ,
             },
-           {
+            {
                 image: this.PR_lamp,
                 layerWidth: 228,
                 positionY: 177 - this.PR_lamp.height,
@@ -168,55 +228,6 @@ export class TitanEffectScene extends AbstractScene {
                 speed: mySpeed * 0.8 ,
             }
         ];
-
-
-
-        data.forEach(layer => {
-            const disp =  time*layer.speed;
-            framebuffer.drawTexture((0 - (disp % layer.layerWidth)+layer.positionX)|0,layer.positionY,  layer.image, 1);
-            framebuffer.drawTexture((layer.layerWidth - (disp % layer.layerWidth)+layer.positionX)|0,layer.positionY, layer.image, 1);
-        });
-
-        this.cinematicScroller(framebuffer, this.texture4, time);
-
     }
-
-    public cinematicScroller(framebuffer: Framebuffer, texture: Texture, time: number): void {
-        const scrollText: Array<string> = [
-            '', '', '', '', '', '', '', '', '', '',
-            '', '', '', '', '', '', '', '', '', '',
-            '', '', '', '', '',
-            'YOU HAVE BEEN WATCHING',
-            '',
-            'D A R K   M A T T E R',
-            '',
-            'A JAVASCRIPT DEMO MADE FOR',
-            'NORDLICHT 2018',
-            '',
-            'CREDITS',
-            '',
-            'CODE BY',
-            'TRIGGER',
-            '',
-            'GRAFICS BY',
-            'PREMIUM',
-            '',
-            'MUSIC BY',
-            'VIRGILL'
-        ];
-        time = time * 0.8;
-
-        const scrollerOffset = Math.round(framebuffer.interpolate(0, 250, time & 0xff) * 8);
-
-        for (let i = 1; i < framebuffer.height / 8; i++) {
-            const text = scrollText[Math.floor((i + (time / 256))) % scrollText.length];
-            const x = (framebuffer.width / 2 - text.length * 8 / 2) | 0;
-            const y = 8 * i - scrollerOffset;
-            // TODO: proper text clipping to rect
-            // maybe just for first and last row
-            framebuffer.drawText(x, y, text, texture);
-        }
-    }
-
 
 }
